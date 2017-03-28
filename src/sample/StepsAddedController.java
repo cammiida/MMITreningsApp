15af0d7ec566bc3a03125843d31498f45389c8b8
cammiida <camillmd@stud.ntnu.no>
2017-03-28 19:24:41 +0200

    Made changes to AddWalk and created StepsAdded

--- src/sample/StepsAddedController.java
+++ src/sample/StepsAddedController.java
@@ -1,32 +1,33 @@
 package sample;
 
-import com.sun.xml.internal.ws.api.FeatureConstructor;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.fxml.FXML;
-import javafx.scene.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AddWalkController implements Initializable{
+public class StepsAddedController {
+
     Stage prevStage;
 
     @FXML
     private Button homeButton;
 
     @FXML
-    private Button addWalkButton;
+    private Button backButton;
 
+    @FXML
+    private Button yesButton;
 
-    @Override
+    @FXML
+    private Button noButton;
+
     public void initialize(URL location, ResourceBundle resources) {
     }
 
@@ -35,18 +36,35 @@ public class AddWalkController implements Initializable{
     }
 
     @FXML
-    public void home (ActionEvent event) throws IOException{
+    public void home(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         stage.setTitle("Hjem");
         FXMLLoader myLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
 
         Parent root = myLoader.load();
-        Scene competitionsScene = new Scene(root);
+        Scene homeScene = new Scene(root);
 
         MainWindowController mainWindowController = myLoader.getController();
         mainWindowController.setPrevStage(stage);
 
-        stage.setScene(competitionsScene);
+        stage.setScene(homeScene);
+        prevStage.close();
+        stage.show();
+    }
+
+    @FXML
+    public void back(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("Hjem");
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("AddWalk.fxml"));
+
+        Parent root = myLoader.load();
+        Scene homeScene = new Scene(root);
+
+        AddWalkController addWalkController = myLoader.getController();
+        addWalkController.setPrevStage(stage);
+
+        stage.setScene(homeScene);
         prevStage.close();
         stage.show();
     }
@@ -85,32 +103,5 @@ public class AddWalkController implements Initializable{
         stage.show();
 
     }
-    @FXML
-    public void setSteps (Integer steps) {
-
-    }
-    @FXML
-    public void setDate (){
-
-    }
-    @FXML
-    public void addWalk () throws IOException{
-        Stage stage = new Stage();
-        stage.setTitle("Legg til skritt");
-        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("StepsAdded.fxml"));
-
-        Parent root = myLoader.load();
-        Scene settingsScene = new Scene(root);
-
-        StepsAddedController stepsAddedController = myLoader.getController();
-        stepsAddedController.setPrevStage(stage);
-
-        stage.setScene(settingsScene);
-        prevStage.close();
-        stage.show();
-    }
-
 
 }
-
-
